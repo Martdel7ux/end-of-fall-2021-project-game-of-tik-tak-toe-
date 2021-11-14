5de88954c02811c6a1903cc74c8f11979f8ce455
Martin Ahoto <martinahoto4@gmail.com>
2021-11-14 09:41:01 +0200

    Initial Commit

---
+++ end of fall 2021 project(game of tik tak toe)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  end of fall 2021 project(game of tik tak toe)
+//
+//  Created by MARTIN on 14/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
